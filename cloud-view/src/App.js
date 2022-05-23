d49c935ecc53ccec7405beb7884b1257973d04a8
ChunChun Huang <cchuang.1024@outlook.com>
2022-05-23 08:12:22 +0800

    temp commit

--- cloud-view/src/App.js
+++ cloud-view/src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {Container, Switch} from "@mui/material";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import moment from 'moment';
+
+const unixToMilli = unix => unix * 1000;
+
+const initOptions = () => {
+    const totalMinutes = (60 * 24) + 1;
+
+    let power = [];
+    let fix = [];
+
+    for (let i = 0; i < totalMinutes; i++) {
+        const date = moment().startOf('day').add(i, 'minutes');
+
+        power.push({
+            x: unixToMilli(date.unix()),
+            y: 0
+        });
+        fix.push({
+            x: unixToMilli(date.unix()),
+            y: null
+        });
+    }
+
+    return ({
+        title: {
+            text: 'Electric Data'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: [{
+            labels: {
+                format: '{value} kW'
+            },
+            title: {
+                text: 'ActivePower'
+            }
+        }, {
+            title: {
+                text: 'FixState'
+            },
+            opposite: true
+        }],
+        legend: {
+            enabled: true
+        },
+        series: [{
+            name: 'ActivePower',
+            type: 'spline',
+            yAxis: 1,
+            data: power,
+        }, {
+            name: 'FixState',
+            type: 'spline',
+            data: fix,
+        }]
+    });
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const now = moment();
+
+    const [auto, setAuto] = useState(true);
+    const updateAuto = event => setAuto(event.target.checked);
+
+    const [options, setOptions] = useState(initOptions());
+
+    useEffect(() => {
+    }, []);
+
+    console.log(options);
+
+    return (
+        <Container maxWidth="xl">
+            <h1>Electric Data of {now.format("YYYY-MM-DD")}</h1>
+            <Switch checked={auto} onChange={updateAuto}/>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
+        </Container>
+    );
 }
 
 export default App;
