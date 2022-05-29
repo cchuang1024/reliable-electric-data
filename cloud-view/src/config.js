5a4d78f12bff4d1997498bf69f8d8e3d0c519380
ChunChun Huang <cchuang.1024@outlook.com>
2022-05-30 07:52:14 +0800

    cloud view url fix

--- cloud-view/src/config.js
+++ cloud-view/src/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://127.0.0.1:38080';
-export const BASE_URL = 'http://dispatch-cloud:8080';
+// export const BASE_URL = 'http://dispatch-cloud:8080';
+export const BASE_URL = '';
