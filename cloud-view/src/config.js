e2b39075c8cb2dd8c6ebcf854ceccdcabf078bb3
ChunChun Huang <cchuang.1024@outlook.com>
2022-07-02 18:44:04 +0800

    fix cloud ip

--- cloud-view/src/config.js
+++ cloud-view/src/config.js
@@ -1,3 +1,4 @@
 // export const BASE_URL = 'http://127.0.0.1:38080';
-export const BASE_URL = 'http://dispatch-cloud:8080';
+// export const BASE_URL = 'http://dispatch-cloud:8080';
+export const BASE_URL = 'http://192.168.0.164:8080';
 // export const BASE_URL = '';
