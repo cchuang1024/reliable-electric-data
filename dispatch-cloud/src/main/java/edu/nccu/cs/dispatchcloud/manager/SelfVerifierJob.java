e5de8f45913765e7b7f85b7b94392ee478f5e069
ChunChun Huang <cchuang.1024@outlook.com>
2022-07-04 07:42:27 +0800

    update SelfVerifyJob

--- dispatch-cloud/src/main/java/edu/nccu/cs/dispatchcloud/manager/SelfVerifierJob.java
+++ dispatch-cloud/src/main/java/edu/nccu/cs/dispatchcloud/manager/SelfVerifierJob.java
@@ -3,12 +3,14 @@ package edu.nccu.cs.dispatchcloud.manager;
 import edu.nccu.cs.dispatchcloud.signedmeterdata.SignedMeterDataEntity;
 import edu.nccu.cs.utils.DateTimeUtils;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -33,13 +35,19 @@ public class SelfVerifierJob implements Runnable {
 
         long dayFirstTimestamp = DateTimeUtils.timestampFromLocalDateTime(LocalDateTime.now().truncatedTo(ChronoUnit.DAYS));
         long nowTimestamp = DateTimeUtils.timestampFromLocalDateTime(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
-
         List<SignedMeterDataEntity> preTimestampUntilNow = querierService.getMeterDataBetweenPreTimestamp(dayFirstTimestamp, nowTimestamp);
+        
+        if(preTimestampUntilNow.isEmpty()){
+            return;
+        }
+
         Set<Long> preTimestamps = preTimestampUntilNow.stream()
                                                       .map(SignedMeterDataEntity::getPreTimestamp)
                                                       .collect(Collectors.toSet());
-
-        List<SignedMeterDataEntity> timestampUntilNow = querierService.getMeterDataBetweenTimestamp(dayFirstTimestamp, nowTimestamp);
+        Long firstPreTimestamp = Collections.min(preTimestamps);
+        Long lastPreTimestamp = Collections.max(preTimestamps);
+        
+        List<SignedMeterDataEntity> timestampUntilNow = querierService.getMeterDataBetweenTimestamp(firstPreTimestamp, lastPreTimestamp);
         Set<Long> timestamps = timestampUntilNow.stream()
                                                 .map(SignedMeterDataEntity::getTimestamp)
                                                 .collect(Collectors.toSet());
