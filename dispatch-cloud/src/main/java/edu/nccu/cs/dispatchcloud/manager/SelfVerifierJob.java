61dc2af178443e77ef576174a9c364b30a9b3625
ChunChun Huang <cchuang.1024@outlook.com>
2022-07-10 00:15:09 +0800

    change self verify to hourly

--- dispatch-cloud/src/main/java/edu/nccu/cs/dispatchcloud/manager/SelfVerifierJob.java
+++ dispatch-cloud/src/main/java/edu/nccu/cs/dispatchcloud/manager/SelfVerifierJob.java
@@ -3,8 +3,8 @@ package edu.nccu.cs.dispatchcloud.manager;
 import edu.nccu.cs.dispatchcloud.signedmeterdata.SignedMeterDataEntity;
 import edu.nccu.cs.utils.DateTimeUtils;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -29,31 +29,36 @@ public class SelfVerifierJob implements Runnable {
     @Autowired
     private QuerierService querierService;
 
+    @Value("${application.verifyPeriod}")
+    private Integer verifyPeriod;
+
     @Override
     public void run() {
         log.info("self verify fixing data.");
 
-        long dayFirstTimestamp = DateTimeUtils.timestampFromLocalDateTime(LocalDateTime.now().truncatedTo(ChronoUnit.DAYS));
-        long nowTimestamp = DateTimeUtils.timestampFromLocalDateTime(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES));
-        log.info("find meter data by preTimestamp from {} to {}", dayFirstTimestamp, nowTimestamp);
+        LocalDateTime currentMinute = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        long verifyEnd = DateTimeUtils.timestampFromLocalDateTime(currentMinute);
+        // long verifyStart = DateTimeUtils.timestampFromLocalDateTime(LocalDateTime.now().truncatedTo(ChronoUnit.DAYS));
+        long verifyStart = DateTimeUtils.timestampFromLocalDateTime(currentMinute.minusHours(verifyPeriod));
+        log.info("find meter data by preTimestamp from {} to {}", verifyStart, verifyEnd);
 
-        List<SignedMeterDataEntity> preTimestampUntilNow = querierService.getMeterDataBetweenPreTimestamp(dayFirstTimestamp, nowTimestamp);
-        if(preTimestampUntilNow.isEmpty()){
+        List<SignedMeterDataEntity> preTimestampUntilNow = querierService.getMeterDataBetweenPreTimestamp(verifyStart, verifyEnd);
+        if (preTimestampUntilNow.isEmpty()) {
             return;
         }
 
         Set<Long> preTimestamps = preTimestampUntilNow.stream()
-                                                      .map(SignedMeterDataEntity::getPreTimestamp)
-                                                      .collect(Collectors.toSet());
+                .map(SignedMeterDataEntity::getPreTimestamp)
+                .collect(Collectors.toSet());
 
         Long firstPreTimestamp = Collections.min(preTimestamps);
         Long lastPreTimestamp = Collections.max(preTimestamps);
         log.info("find meter data by timestamp from {} to {}", firstPreTimestamp, lastPreTimestamp);
-        
+
         List<SignedMeterDataEntity> timestampUntilNow = querierService.getMeterDataBetweenTimestamp(firstPreTimestamp, lastPreTimestamp);
         Set<Long> timestamps = timestampUntilNow.stream()
-                                                .map(SignedMeterDataEntity::getTimestamp)
-                                                .collect(Collectors.toSet());
+                .map(SignedMeterDataEntity::getTimestamp)
+                .collect(Collectors.toSet());
 
         preTimestamps.removeAll(timestamps);
 
