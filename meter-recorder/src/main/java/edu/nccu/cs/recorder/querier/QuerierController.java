43b61a934cf822dc757ed637ac3a1aacf3b04a9a
ChunChun Huang <cchuang.1024@outlook.com>
2022-07-12 07:30:56 +0800

    fix controller annotation

--- meter-recorder/src/main/java/edu/nccu/cs/recorder/querier/QuerierController.java
+++ meter-recorder/src/main/java/edu/nccu/cs/recorder/querier/QuerierController.java
@@ -11,11 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 import java.util.Optional;
 
-@Controller
+@RestController
 @Slf4j
 public class QuerierController {
 
